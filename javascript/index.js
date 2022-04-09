84f2922ed01a9aa9f74a2e23c0fc76d7237d1525
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-04-09 18:58:48 +0200

    [FEAT]: add active menu

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,14 @@
 const menuIcon = document.querySelector('div.menu-icon');
 const bars = document.querySelector('i.fa-bars');
 const xmark = document.querySelector('i.fa-xmark');
+const nav = document.querySelector('nav.nav');
+const header = document.querySelector('header.header');
+const main = document.querySelector('main.main');
 
 menuIcon.addEventListener('click', () => {
-  bars.classList.toggle('move-bars');
-  xmark.classList.toggle('move-xmark');
+  bars.classList.toggle('js-bars');
+  xmark.classList.toggle('js-xmark');
+  nav.classList.toggle('js-nav');
+  header.classList.toggle('js-header');
+  main.classList.toggle('js-main');
 });
