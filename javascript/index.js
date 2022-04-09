362e5c0158457394cc7d2b8de42bd14ec53603ce
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-04-09 13:21:29 +0200

    [FEAT]: add header button effect by JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,8 @@
 const menuIcon = document.querySelector('div.menu-icon');
+const bars = document.querySelector('i.fa-bars');
+const xmark = document.querySelector('i.fa-xmark');
 
-menuIcon.addEventListener('click', () => {});
+menuIcon.addEventListener('click', () => {
+  bars.classList.toggle('move-bars');
+  xmark.classList.toggle('move-xmark');
+});
