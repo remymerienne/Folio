25c138d2f2cbccf789a55df76703a0d7c17d8d90
Rémy Merienne <remy.merienne-dev@outlook.fr>
2022-04-10 13:11:59 +0200

    [FEAT]: add modal system when opening navigation

--- javascript/index.js
+++ javascript/index.js
@@ -3,12 +3,12 @@ const bars = document.querySelector('i.fa-bars');
 const xmark = document.querySelector('i.fa-xmark');
 const nav = document.querySelector('nav.nav');
 const header = document.querySelector('header.header');
-const main = document.querySelector('main.main');
+const modal = document.querySelector('div.modal');
 
 menuIcon.addEventListener('click', () => {
   bars.classList.toggle('js-bars');
   xmark.classList.toggle('js-xmark');
   nav.classList.toggle('js-nav');
   header.classList.toggle('js-header');
-  main.classList.toggle('js-main');
+  modal.classList.toggle('js-modal');
 });
